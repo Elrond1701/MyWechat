68c20a6295e361f095b2eea6e29988f9c8835c95
平安喜乐 <fanxl01@users.noreply.github.com>
2021-05-25 15:13:58 +0800

    Revert "chat_list"
    This reverts commit 08ed840d3bcfec355ebd8a670a52f55f66fb756f.

--- app/src/main/java/com/example/mywechat/data/Chat.java
+++ app/src/main/java/com/example/mywechat/data/Chat.java
@@ -3,19 +3,13 @@ package com.example.mywechat.data;
 import java.lang.String;
 
 public class Chat {
-//    private  int Profile; // 头像
+    private  int Profile; // 头像
     private String Nickname; // 昵称
     private String LastSpeak; //最后聊天内容
     private String LastSpeakTime; //最后联络时间
 
-    public Chat(String Nickname, int Profile, String LastSpeak, String LastSpeakTime) {
-        this.Nickname = Nickname;
-//        this.Profile = Profile;
-        this.LastSpeak = LastSpeak;
-        this.LastSpeakTime = LastSpeakTime;
-    }
 
-//    public int getProfile() { return Profile; }
+    public int getProfile() { return Profile; }
 
     public String getLastSpeak() { return LastSpeak; }
 
